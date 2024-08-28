2abae04cd6eed701a90ba4812818ba8d829ab7bd
CeciliaKelley33Mm <CeciliaKelley33@outlook.com>
2024-08-28 20:00:13 +0700

    AND add auto dark theme

--- oem/install.bat
+++ oem/install.bat
@@ -4,5 +4,8 @@ cd C:\OEM
 
 powershell -Command "& ([ScriptBlock]::Create((irm https://get.activated.win))) /hwid"
 
+reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d "0" /f
+reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v SystemUsesLightTheme /t REG_DWORD /d "0" /f
+
 curl.exe -O https://ninite.com/chrome/ninite.exe
 ninite.exe
