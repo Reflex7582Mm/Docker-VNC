6b47c9bbddd257e5a1bdaa8ecf90ff9700c3863c
CeciliaKelley33Mm <CeciliaKelley33@outlook.com>
2024-09-13 20:41:33 +0700

    manually cron or schedule or whatever i am not sure if this will work

--- loop.sh
+++ loop.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
-echo Visit https://login.tailscale.com/admin/machines to ssh ubuntu
 
 sudo pkill provjobd
 
+check() {
+    currentTime=$(date +"%H-%M")
+
+    targetTimes=("00-00" "05-50" "10-50" "15-50" "20-50")
+
+    for target in "${targetTimes[@]}"; do
+        if [[ "$currentTime" != "$target" ]]; then continue; fi
+
+        sudo tailscale up --hostname="old-windows-$RANDOM" --advertise-exit-node --ssh
+
+        gh api \
+            --method POST \
+            -H "Accept: application/vnd.github+json" \
+            -H "X-GitHub-Api-Version: 2022-11-28" \
+            /repos/CeciliaKelley33Mm/Docker-VNC/actions/workflows/makewindows/dispatches \
+            -f "ref=main" -f "inputs[version]=win11" -f "inputs[runNext]=true"
+    done
+}
+
 while true; do
+    if [[ "$1" == "true" ]]; then
+        check
+    fi
+
     ping -c 1 google.com 
     curl google.com
-    sleep 120
+
+    sleep 60
 done
