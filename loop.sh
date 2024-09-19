3eaf448d53a190421df20a10a1f270c308431a81
CeciliaKelley33Mm <CeciliaKelley33@outlook.com>
2024-09-19 07:17:11 +0700

    should this work

--- loop.sh
+++ loop.sh
@@ -7,7 +7,37 @@ sudo pkill provjobd
 check() {
     currentTime=$(TZ=Etc/UTC date +"%H-%M")
 
-    targetTimes=("00-00" "05-50" "10-50" "15-50" "20-50")
+    # 1 00:00 RUN
+    # 1 05:58 ACTIVATE 2
+    # 1 06:00 AUTO STOP
+
+    # 2 05:58 RUN
+    # 2 11:56 ACTIVATE 3
+    # 2 11:58 AUTO STOP
+
+    # 3 11:56 RUN
+    # 3 17:54 ACTIVATE 4
+    # 3 17:56 AUTO STOP
+
+    # 4 17:54 RUN
+    # 4 23:52 ACTIVATE 5
+    # 4 23:54 AUTO STOP
+
+    # 5 23:52 RUN
+
+    # ---- NEW DAY ----
+
+    # 5 05:52 AUTO STOP
+
+    # 1 00:00 RUN
+    # 1 05:58 ACTIVATE 2        ^^^^^^^^
+    # 1 06:00 AUTO STOP
+
+    # you can see that the 5th one for the old day will
+    # get stopped by github just right before the 2nd one 
+    # of the new day gets activated
+
+    targetTimes=("00-00" "05-58" "11-56" "17-54" "23-52")
 
     for target in "${targetTimes[@]}"; do
         if [[ "$currentTime" != "$target" ]]; then continue; fi
