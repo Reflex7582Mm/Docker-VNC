cdf08b41cd1978f059e182d7e5d3f70acc7d6bce
CeciliaKelley33Mm <CeciliaKelley33@outlook.com>
2024-09-20 08:51:46 +0700

    some workaround

--- oem/install.bat
+++ oem/install.bat
@@ -8,8 +8,11 @@ REM this sets the color to dark. works on windows 10 & windows 11
 reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d "0" /f
 reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v SystemUsesLightTheme /t REG_DWORD /d "0" /f
 
-REM sets the windows 11 dark theme
-start "" "C:\Windows\Resources\Themes\dark.theme"
+REM sets the windows 11 dark theme, i used this cmd workaround
+REM because if you're on windows 10, the theme won't exist
+REM and it will just show an error prompt which is interactive
+REM which also blocks the next command from running
+start cmd /c start "" "C:\Windows\Resources\Themes\dark.theme"
 
 reg add "HKCU\Control Panel\Desktop" /v LogPixels /t REG_DWORD /d 144 /f
 reg add "HKCU\Control Panel\Desktop" /v Win8DpiScaling /t REG_DWORD /d 1 /f
