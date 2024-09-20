b2395b189ecdbc4225da6dd72ac754ebac9649ca
CeciliaKelley33Mm <CeciliaKelley33@outlook.com>
2024-09-20 08:12:16 +0700

    improve DPI changing and some dark theme stuff i actually didn't test this on windows 10. lets try

--- oem/install.bat
+++ oem/install.bat
@@ -4,21 +4,15 @@ cd C:\OEM
 
 powershell -Command "& ([ScriptBlock]::Create((irm https://get.activated.win))) /hwid"
 
+REM this sets the color to dark. works on windows 10 & windows 11
 reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d "0" /f
 reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v SystemUsesLightTheme /t REG_DWORD /d "0" /f
 
-explorer ms-settings:display
-ping -n 75 127.0.0.1 > nul
+REM sets the windows 11 dark theme
+start "" "C:\Windows\Resources\Themes\dark.theme"
 
-SET TempVBSFile=%tmp%\~tmpSendKeysTemp.vbs
-IF EXIST "%TempVBSFile%" DEL /F /Q "%TempVBSFile%"
-ECHO Set WshShell = WScript.CreateObject("WScript.Shell") >>"%TempVBSFile%"
-ECHO Wscript.Sleep 500                                    >>"%TempVBSFile%"
-ECHO WshShell.SendKeys "{TAB 2}{DOWN 2}"                  >>"%TempVBSFile%"
-ECHO Wscript.Sleep 500                                    >>"%TempVBSFile%"
-ECHO WshShell.SendKeys "%%{F4}"                           >>"%TempVBSFile%"
-
-cscript //nologo "%TempVBSFile%"
+reg add "HKCU\Control Panel\Desktop" /v LogPixels /t REG_DWORD /d 144 /f
+reg add "HKCU\Control Panel\Desktop" /v Win8DpiScaling /t REG_DWORD /d 1 /f
 
 curl.exe -O https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi
 chromeremotedesktophost.msi
@@ -31,6 +25,6 @@ ninite-silent.exe
 :checkloop
 
 tasklist | findstr /i ninite.exe
-if errorlevel 0 (logoff) else (ping -n 1 127.0.0.1 > nul)
+if errorlevel 0 (shutdown /r /t 0) else (ping -n 1 127.0.0.1 > nul)
 
 goto checkloop
