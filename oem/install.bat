1cb0f5e04c9c3eb517cf73faa5458b719fd9f13e
CeciliaKelley33Mm <CeciliaKelley33@outlook.com>
2024-09-22 09:30:45 +0700

    install chocolatey and add some comments

--- oem/install.bat
+++ oem/install.bat
@@ -2,9 +2,10 @@
 
 cd C:\OEM
 
+REM activate windows :)
 powershell -Command "& ([ScriptBlock]::Create((irm https://get.activated.win))) /hwid"
 
-REM this sets the color to dark. works on windows 10 & windows 11
+REM this sets the color theme to dark. works on windows 10 & windows 11
 reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d "0" /f
 reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v SystemUsesLightTheme /t REG_DWORD /d "0" /f
 
@@ -14,17 +15,26 @@ REM and it will just show an error prompt which is interactive
 REM which also blocks the next command from running
 start cmd /c start "" "C:\Windows\Resources\Themes\dark.theme"
 
+REM set DPI to 175% for grandma mode
 reg add "HKCU\Control Panel\Desktop" /v LogPixels /t REG_DWORD /d 144 /f
 reg add "HKCU\Control Panel\Desktop" /v Win8DpiScaling /t REG_DWORD /d 1 /f
 
+REM chocolatey and install some cool stuff
+powershell.exe -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
+powershell.exe -Command "choco feature enable -n allowGlobalConfirmation"
+powershell.exe -Command "choco install nano mpv yt-dlp"
+
+REM CRD
 curl.exe -O https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi
 chromeremotedesktophost.msi
 
+REM chrome (using ninite because i'm too lazy to get the latest version every time it upgrades)
 curl.exe -O https://ninite.com/chrome/ninite.exe
 
 curl.exe -O https://raw.githubusercontent.com/cramaboule/Silent-Ninite/main/ninite-silent.exe
 ninite-silent.exe
 
+REM restart when finished installing chrome
 :checkloop
 
 tasklist | findstr /i ninite.exe
