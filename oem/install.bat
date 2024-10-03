730d8d681dc3122f2c7082ad26f07e4427059282
CeciliaKelley33Mm <CeciliaKelley33@outlook.com>
2024-10-03 12:40:07 +0700

    add opennbs and powershell core (7)

--- oem/install.bat
+++ oem/install.bat
@@ -34,7 +34,7 @@ REM ---               ^^^^
 
 choco feature enable -n allowGlobalConfirmation
 choco feature disable -n checksumFiles
-choco install nano mpv yt-dlp ffmpeg googlechrome chrome-remote-desktop-host
+choco install nano mpv yt-dlp ffmpeg googlechrome chrome-remote-desktop-host opennbs powershell-core
 
 ping -n 3 127.0.0.1
 
